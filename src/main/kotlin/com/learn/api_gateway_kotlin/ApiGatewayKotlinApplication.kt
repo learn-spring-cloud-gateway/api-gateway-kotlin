4c20bcb42154c8680acce77ad429ebc4ecd71b80
aassalax <aassali.dev@gmail.com>
2024-07-04 23:47:58 +0200

    first commit

---
+++ src/main/kotlin/com/learn/api_gateway_kotlin/ApiGatewayKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.learn.api_gateway_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/learn/api_gateway_kotlin/ApiGatewayKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learn.api_gateway_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
